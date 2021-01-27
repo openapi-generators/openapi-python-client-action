70339fc8ae7064fde4b7d4306f29d779c8c06622
Ethan Mann <emann@triaxtec.com>
2021-01-27 11:57:36 -0500

    Switched to using pipx for running openapi-python-client

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM python:3.8
 
+RUN python -m pip install --upgrade pip
+RUN pip install pipx
+
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,11 @@
-#!/bin/sh -l
+#!/bin/bash
 
 openapi_python_client_version=$1
 openapi_file_path=$2
 config_file_path=$3
 
-pip install openapi-python-client==${openapi_python_client_version}
-
 if [[ "$config_file_path" == "NOT_SPECIFIED" ]]; then
-    python -m openapi-python-client generate --path ${openapi_file_path}
+    pipx run --spec openapi-python-client==${openapi_python_client_version} openapi-python-client generate --path ${openapi_file_path}
 else
-    python -m openapi-python-client generate --path ${openapi_file_path} --config ${config_file_path}
+    pipx run --spec openapi-python-client==${openapi_python_client_version} openapi-python-client --config ${config_file_path} generate --path ${openapi_file_path}
 fi
