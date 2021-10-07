c090961ea2d26f79ab252ed7ca79e2be31d2ddd8
Dylan Anthony <contact@dylananthony.com>
2021-10-06 20:30:42 -0600

    fix: Black and isort would not run properly.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
 openapi_python_client_version=$1
 openapi_file_path=$2
@@ -6,20 +8,21 @@ openapi_url=$3
 config_file_path=$4
 
 if [[ "$openapi_python_client_version" != "NOT_SPECIFIED" ]]; then
-    version_arg="--spec openapi-python-client==${openapi_python_client_version}"
+    version_arg="openapi-python-client==${openapi_python_client_version}"
 else
-    version_arg=""
+    version_arg="openapi-python-client"
 fi
 
 if [[ "$config_file_path" != "NOT_SPECIFIED" ]]; then
-    config_arg="--config ${config_file_path}"
+    config_arg="--config=${config_file_path}"
 else
     config_arg=""
 fi
 
-openapi_document_path_or_url_arg="--path ${openapi_file_path}"
+openapi_document_path_or_url_arg="--path=${openapi_file_path}"
 if [[ "$openapi_url" != "NOT_SPECIFIED" ]]; then
-    openapi_document_path_or_url_arg="--url ${openapi_url}"
+    openapi_document_path_or_url_arg="--url=${openapi_url}"
 fi
 
-pipx run ${version_arg} openapi-python-client generate ${config_arg} ${openapi_document_path_or_url_arg}
+pipx install "${version_arg}" --include-deps
+openapi-python-client generate ${config_arg} ${openapi_document_path_or_url_arg}
